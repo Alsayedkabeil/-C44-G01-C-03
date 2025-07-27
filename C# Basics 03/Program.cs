ef7440f96825b02c7369dd7ea066afb49df24a05
السيد السعيد على قابيل stu <323230190@sha.edu.eg>
2025-07-27 22:26:34 +0300

    Q2

--- C# Basics 03/Program.cs
+++ C# Basics 03/Program.cs
@@ -8,12 +8,23 @@
 
             #region Q1
 
-            Console.WriteLine(  "Enter Your Name: ");
+            //Console.WriteLine(  "Enter Your Name: ");
 
-            String Name = Console.ReadLine();
+            //String Name = Console.ReadLine();
 
-            Console.WriteLine(  $"Your Name Is: {Name}");
+            //Console.WriteLine(  $"Your Name Is: {Name}");
 
+            #endregion
+
+            #region Q2
+
+   
+
+            string input = "seka";
+            int number = Convert.ToInt32(input);
+            Console.WriteLine($"Converted number: {number}");
+
+            // Output: Unhandled exception. System.FormatException: The input string 'seka' was not in a correct format.
             #endregion
 
 
