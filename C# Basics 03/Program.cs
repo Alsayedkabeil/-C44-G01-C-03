8ca344f27deb6c530a762ad43bbcc9b9d1eda8b1
السيد السعيد على قابيل stu <323230190@sha.edu.eg>
2025-07-27 23:22:40 +0300

    Q4

--- C# Basics 03/Program.cs
+++ C# Basics 03/Program.cs
@@ -29,21 +29,28 @@
 
             #region Q3
 
-            float P1 =(float) 1.40 ;
-            float P2 = (float)1.50;
-            float Result = P1 + P2;
-            Console.WriteLine(  Result);
+            //float P1 =(float) 1.40 ;
+            //float P2 = (float)1.50;
+            //float Result = P1 + P2;
+            //Console.WriteLine(  Result);
 
-            //OR
-            float P01 = 2.40f;
-            float P02 = 2.50f;
-            float Result02 = P1 + P2;
+            ////OR
+            //float P01 = 2.40f;
+            //float P02 = 2.50f;
+            //float Result02 = P1 + P2;
 
-            Console.WriteLine(Result02);
+            //Console.WriteLine(Result02);
 
 
             //output is 2.9 // Explicit Casting
 
+
+            #endregion
+
+            #region Q4
+            string x = "think before u click  ";
+          
+            Console.WriteLine(x.Substring(5));
 
             #endregion
 
