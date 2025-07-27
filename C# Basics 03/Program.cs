502dfe69c56304558849278126fb2e562a2aefec
السيد السعيد على قابيل stu <323230190@sha.edu.eg>
2025-07-27 23:15:34 +0300

    Q3

--- C# Basics 03/Program.cs
+++ C# Basics 03/Program.cs
@@ -20,13 +20,32 @@
 
    
 
-            string input = "seka";
-            int number = Convert.ToInt32(input);
-            Console.WriteLine($"Converted number: {number}");
+            //string input = "seka";
+            //int number = Convert.ToInt32(input);
+            //Console.WriteLine($"Converted number: {number}");
 
             // Output: Unhandled exception. System.FormatException: The input string 'seka' was not in a correct format.
             #endregion
 
+            #region Q3
+
+            float P1 =(float) 1.40 ;
+            float P2 = (float)1.50;
+            float Result = P1 + P2;
+            Console.WriteLine(  Result);
+
+            //OR
+            float P01 = 2.40f;
+            float P02 = 2.50f;
+            float Result02 = P1 + P2;
+
+            Console.WriteLine(Result02);
+
+
+            //output is 2.9 // Explicit Casting
+
+
+            #endregion
 
         }
     }
