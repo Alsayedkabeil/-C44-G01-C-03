e1f64637e0bc6c39484b76b754a73618d8bb1209
السيد السعيد على قابيل stu <323230190@sha.edu.eg>
2025-07-27 21:47:18 +0300

    Q1

--- C# Basics 03/Program.cs
+++ C# Basics 03/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-          
+            //Part 01
+
+            #region Q1
+
+            Console.WriteLine(  "Enter Your Name: ");
+
+            String Name = Console.ReadLine();
+
+            Console.WriteLine(  $"Your Name Is: {Name}");
+
+            #endregion
 
 
         }
