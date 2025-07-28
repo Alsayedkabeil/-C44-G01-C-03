70bd6f6c3a077af6ccb6a18744ee522067bcb1af
السيد السعيد على قابيل stu <323230190@sha.edu.eg>
2025-07-29 00:42:54 +0300

    Q4

--- C# Basics 03/Program.cs
+++ C# Basics 03/Program.cs
@@ -215,21 +215,32 @@ namespace C__Basics_03
 
             #region Q3
 
-           
-            Console.WriteLine(  "Enter number01: ");
-            int number01 =int.Parse( Console.ReadLine() );
-            Console.WriteLine("Enter number02: ");
-            int number02 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter number03: ");
-            int number03 = int.Parse(Console.ReadLine());
 
-            int max = Math.Max(number01,Math.Max(number02,number03));
-            int min = Math.Min(number01, Math.Min(number02, number03));
-            Console.WriteLine(  $"MAx Element is {max}");
-            Console.WriteLine($"Min Element is : {min}  ");
+            //Console.WriteLine(  "Enter number01: ");
+            //int number01 =int.Parse( Console.ReadLine() );
+            //Console.WriteLine("Enter number02: ");
+            //int number02 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter number03: ");
+            //int number03 = int.Parse(Console.ReadLine());
+
+            //int max = Math.Max(number01,Math.Max(number02,number03));
+            //int min = Math.Min(number01, Math.Min(number02, number03));
+            //Console.WriteLine(  $"MAx Element is {max}");
+            //Console.WriteLine($"Min Element is : {min}  ");
+
             #endregion
+            #region Q4
+            Console.WriteLine( "Enter num ");
 
+            int num = int.Parse( Console.ReadLine() );
 
+            if (num % 2 == 0)
+            {
+                Console.WriteLine(" is even ");
+
+            }
+            else { Console.WriteLine(   " is odd "); }
+            #endregion
 
         }
     }
