b0e672f98416c73a2fb2bab10611974cae6256e1
السيد السعيد على قابيل stu <323230190@sha.edu.eg>
2025-07-29 00:33:54 +0300

    Q3

--- C# Basics 03/Program.cs
+++ C# Basics 03/Program.cs
@@ -227,10 +227,6 @@ namespace C__Basics_03
             int min = Math.Min(number01, Math.Min(number02, number03));
             Console.WriteLine(  $"MAx Element is {max}");
             Console.WriteLine($"Min Element is : {min}  ");
-
-           
-
-
             #endregion
 
 
